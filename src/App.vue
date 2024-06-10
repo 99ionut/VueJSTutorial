574b0aa8bf919de79506c105facb6a0cdd701eaa
Ionut <73752549+99ionut@users.noreply.github.com>
2024-06-10 11:45:13 +0200

    Converted static project file to Vue project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <navbar
+        :pages="pages"
+        :active-page="activePage"
+        :nav-link-click="(index) => activePage = index"
+    ></navbar>
+
+    
+    <page-viewer 
+        :page="pages[activePage]">
+    </page-viewer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageViewer from './components/PageViewer.vue';
+import Navbar from './components/Navbar.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+      Navbar,
+      PageViewer
+    },
+    data(){
+        return{
+            activePage: 0,
+            pages: [
+            {
+                link: {text:"Home", url: "index.html"},
+                title:"Hello, Home",
+                content:"content hello home"
+            },
+            {   link: {text:"About", url: "about.html"},
+                title:"Hello, About",
+                content:"content hello About"
+            },
+            {
+                link: {text:"Contact", url: "contact.html"},
+                title:"Hello, Contact",
+                content:"content hello heContactContactllo"
+            }
+            ]
+        };
+    },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav 
+        :class="[`bg-${theme}`, `navbar-${theme}`, 'navbar', 'container', 'navbar-expand-lg']">
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li v-for="(page, index) in pages" class="nav-item active" :key="index">
+            <a 
+                class="nav-link" 
+                :class="{active: activePage == index}"
+                :href="page.link.url"
+                :title="`This link goes to ${page.link.text} page`"
+                @click.prevent="navLinkClick(index)"
+                >
+                {{page.link.text}}
+                </a>
+            </li>
+        </ul>
+        </div>
+        <button 
+            @click="changeTheme"
+            >Change mode</button>
+    </nav>
+</template>
+<script>
+export default {
+    props: ["pages","activePage", "navLinkClick"],
+    data(){
+        return{
+            theme: "dark"
+        }
+    },
+    methods:{
+        changeTheme(){
+            console.log("change theme");
+            this.theme = (this.theme == 'light' ? 'dark' : 'light');
+        }
+    }
+}
+</script>
+
+
