4e08c966fe794d8e8f219c3eb3da766d32de6d41
Ionut <73752549+99ionut@users.noreply.github.com>
2024-06-10 16:39:42 +0200

    Validating form live dynamically

--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="container mb-3">
     <form action="">
-      <div class="mb-3">
-        <label for="" class="form-label"> Page Title </label>
-        <input type="text" class="form-control" v-model="pageTitle" />
+      <div class="col">
+        <div class="mb-3">
+          <label for="" class="form-label"> Page Title </label>
+          <input type="text" class="form-control" v-model="pageTitle" />
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label"> content </label>
+          <textarea
+            type="text"
+            class="form-control"
+            rows="5"
+            v-model="content"
+          ></textarea>
+        </div>
       </div>
-      <div class="mb-3">
-        <label for="" class="form-label"> content </label>
-        <textarea type="text" class="form-control" rows="5" v-model="content"></textarea>
+      <div class="col">
+        <div class="mb-3">
+          <label for="" class="form-label"> Link Text </label>
+          <input type="text" class="form-control" v-model="linkText" />
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label"> Link URL </label>
+          <input type="text" class="form-control" v-model="linkUrl" />
+        </div>
+        <div class="mb-3">
+          <div class="form-check">
+            <input for="" class="form-check-input" type="checkbox" />
+            <label for="gridCheck1" class="orm-check-label">Published</label>
+          </div>
+        </div>
       </div>
+
       <div class="mb-3">
         <button
           class="btn btn-primary"
-          @click.prevent="pageCreated({ pageTitle, content })"
+          @click.prevent="submitForm"
+          :disabled="isFormInvalid"
         >
           Create Page
         </button>
@@ -28,7 +53,28 @@ export default {
     return {
       pageTitle: "",
       content: "",
+      linkText: "",
+      linkUrl: "",
     };
+  },
+  computed: {
+    isFormInvalid() {
+      return !this.pageTitle || !this.content || !this.linkText || !this.linkUrl;
+    },
+  },
+  methods: {
+    submitForm() {
+      if (!this.pageTitle || !this.content || !this.linkText || !this.linkUrl) {
+        alert("fill the form");
+        return;
+      }
+
+      this.pageCreated({
+        pageTitle: this.pageTitle,
+        content: this.content,
+        link: { text: this.linkText, url: this.linkUrl },
+      });
+    },
   },
 };
 </script>
