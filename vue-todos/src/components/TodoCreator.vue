03e6eddcb546277a7eaf9f241e883697d712bc40
Ionut <73752549+99ionut@users.noreply.github.com>
2024-06-12 15:35:57 +0200

    dynamic classes binding

--- vue-todos/src/components/TodoCreator.vue
+++ vue-todos/src/components/TodoCreator.vue
@@ -22,11 +22,10 @@ const createTodo = () =>{
 
 </script>
 <template lang="">
-    <div class="input-wrap">
+    <div class="input-wrap" :class="{'input-err': todoState.invalid}">
         <input type="text" v-model="todoState.todo">
         <button @click="createTodo()">Create</button>
     </div>
-    <p v-if="todoState.invalid" class="err-msg">{{ todoState.errMsg}}</p>
     <p v-show="todoState.invalid" class="err-msg">{{ todoState.errMsg}}</p>
 </template>
 
@@ -35,6 +34,10 @@ const createTodo = () =>{
   display: flex;
   transition: 250ms ease;
   border: 2px solid #41b080;
+
+  &.input-err {
+    border-color: red;
+  }
 
   &:focus-within {
     box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
@@ -55,6 +58,12 @@ const createTodo = () =>{
     padding: 8px 16px;
     border: none;
   }
+}
+.err-msg {
+    margin-top: 6px;
+    font-size: 12px;
+    text-align: center;
+    color: red;
 }
 </style>
 
