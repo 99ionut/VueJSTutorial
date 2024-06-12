5bbf4526402d530ccbafebd84b98f711e11ff93c
Ionut <73752549+99ionut@users.noreply.github.com>
2024-06-12 16:49:10 +0200

    passing props to children, v-else-if and v-else

--- vue-todos/src/views/TodosView.vue
+++ vue-todos/src/views/TodosView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { ref } from 'vue'
 import {uid} from "uid";
+import { Icon } from "@iconify/vue";
 import TodoCreator from '../components/TodoCreator.vue'
+import TodoItem from '../components/TodoItem.vue'
 
 const todoList = ref([]);
 
@@ -19,7 +21,12 @@ const createTodo = (todo) =>{
   <main>
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo"/>
-    {{ todoList }}
+    <ul class="todo-list">
+      <TodoItem v-for="todo in todoList" :todo="todo"/>
+    </ul>
+    <p v-if="todoList.length == 0" class="todos-msg">
+      <span>you have no todos</span>
+    </p>
   </main>
 </template>
 
@@ -35,6 +42,22 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
